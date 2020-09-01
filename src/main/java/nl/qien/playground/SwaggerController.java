4544e98d289aa635b8531ea01672e0bbd5eee187
Alex <alexvanmanen@hotmail.com>
2020-09-01 13:46:54 +0200

    swagger-ui added

---
+++ src/main/java/nl/qien/playground/SwaggerController.java
@@ -0,0 +1,13 @@
+package nl.qien.playground;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SwaggerController {
+
+    @RequestMapping("/swagger")
+    public String greeting() {
+        return "redirect:/swagger-ui.html";
+    }
+}
